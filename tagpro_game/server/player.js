0ff74fc1e76bd9b20e2ee09ad5a3f25a2aa06cd1
Seif Ibrahim <seif@debian>
2019-02-12 15:55:53 -0800

    implemented a player for the server which handles physics but not drawn and also contains socket info

--- tagpro_game/server/player.js
+++ tagpro_game/server/player.js
@@ -1,7 +1,59 @@
 export default class Player {
-  constructor(id) {
+  constructor(id, socket, map, { x = 100, y = 100, radius = 20,
+    color = 'blue', controls = 'arrows', acceleration = 0.1 } = {}) {
+
     this.id = id;
+    this.socket = socket;
+
+    this.spawnPosition = { x: x, y: y }
     this.x = 0 
     this.y = 0 
+    this.dx = 0
+    this.dy = 0
+    this.radius = radius
+    this.color = color
+    this.playerImg = this.loadPlayerImage()
+    this.controls = controls
+    this.map = map
+    this.hasFlag = false
+    this.acceleration = acceleration
+    this.frozen = false
   }
+
+  accelerate(keys){
+    let acceleration = this.acceleration
+    if (!this.frozen) {
+      if (keys.leftArrow) { this.dx-= acceleration }
+      if (keys.upArrow) { this.dy-= acceleration }
+      if (keys.rightArrow) { this.dx+= acceleration }
+      if (keys.downArrow) { this.dy+= acceleration }
+    }
+  }
+
+  decelerate() {
+    let drag = 0.975
+    this.dx *= drag
+    this.dy *= drag
+  }
+
+  move(keys) {
+    this.accelerate(keys)
+    this.decelerate()
+    this.x += this.dx
+    this.y += this.dy
+    this.wallCollision()
+  }
+
+  wallCollision() {
+    let thetas = [...Array(72).keys()].map(x => x * 5 * (Math.PI / 180))
+    let force = 0.1
+
+    thetas.forEach(theta => {
+      if (this.map.isWallCollision(this.x + (this.radius * Math.cos(theta)), this.y + (this.radius * Math.sin(theta)))) {
+	this.dx -= force * Math.cos(theta)
+	this.dy -= force * Math.sin(theta)
+      }
+    })
+  }
+}
 }
