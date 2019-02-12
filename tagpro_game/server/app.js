2472b16deabc083285ccc29823a6eb98991d5485
Seif Ibrahim <seif@debian>
2019-02-12 15:54:17 -0800

    moved code from app.js to game-server.js

--- tagpro_game/server/app.js
+++ tagpro_game/server/app.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
-import SocketIO from 'socket.io'
-
-import Game from './game'
-import Player from './player'
+import SocketIO from 'socket.io';
+import GameServer from 'game-server';
 
 const app = express();
 const server = http.Server(app);
@@ -15,38 +13,25 @@ server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+//point static resources and index.html at client directory
 app.use(express.static(path.resolve(__dirname + '/../client')))
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
 
-
-let Games = [];
+//handle socket.io connections
+let gameServer = new GameServer(io);
 
 io.on('connection', (socket) => {
   socket.emit('showMenu');
+
   socket.on('createRequest', () => {
-    const thisGameId = (Math.random() * 100000) | 0; //TODO make sure no collisions happen
-    let game = new Game(thisGameId);
-    const thisPlayerId = (Math.random() * 100000) | 0; //TODO make sure no collisions happen
-    game.players.push(new Player(thisPlayerId));
-    Games[thisGameId] = game;
-    socket.emit('newGameCreated', {gameId: thisGameId, playerId: thisPlayerId});
-    socket.join(thisGameId.toString());
+    gameServer.addGame(socket);
   });
+
   socket.on('joinRequest', (data) => {
-    let game = Games[data.gameId];
-    if(game == undefined) {
-      socket.emit('joinError', {message: "This room does not exist."});
-    } else if(game.isFull()) {
-      socket.emit('joinError', {message: "This room is full."});
-    } else {
-      const thisPlayerId = (Math.random() * 100000) | 0; //DRY
-      game.players.push(new Player(thisPlayerId));
-      socket.join(data.gameId);
-      data.playerId = thisPlayerId;
-      io.to(data.gameId).emit('playerJoined', data);
-    }
+    gameServer.joinGame(data.gameId, socket);
   });
+
 });
