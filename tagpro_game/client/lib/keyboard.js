00e08da2e8385c92ee70d9e25fc47b7bf2c6245a
Seif Ibrahim <seif@debian>
2019-02-18 18:38:56 -0800

    disabled arrow key scrolling

--- tagpro_game/client/lib/keyboard.js
+++ tagpro_game/client/lib/keyboard.js
@@ -1,71 +1,80 @@
 export default class Keyboard {
-  constructor(tagpro_socket) {
-    this.tagpro_socket = tagpro_socket
-    this.keys = {
-      "leftArrow": false,
-      "upArrow": false,
-      "rightArrow": false,
-      "downArrow": false,
-    }
-  }
+	constructor(tagpro_socket) {
+		this.tagpro_socket = tagpro_socket
+		this.keys = {
+			"leftArrow": false,
+			"upArrow": false,
+			"rightArrow": false,
+			"downArrow": false,
+		}
+	}
 
 
-  listenForEvents() {
-    window.addEventListener('keydown', this.onKeyDown.bind(this))
-    window.addEventListener('keyup', this.onKeyUp.bind(this))
-    return this
-  }
+	listenForEvents() {
+		window.addEventListener('keydown', this.onKeyDown.bind(this))
+		window.addEventListener('keyup', this.onKeyUp.bind(this))
+		return this
+	}
 
-  isKeyPressed() {
-    let flag = false
-    for (let prop in this.keys) {
-      if (this.keys[prop]) {
-        flag = true
-      }
-    }
-    return flag
-  }
+	isKeyPressed() {
+		let flag = false
+		for (let prop in this.keys) {
+			if (this.keys[prop]) {
+				flag = true
+			}
+		}
+		return flag
+	}
 
-  onKeyDown(event){
-    let that = this.keys
-    switch (event.keyCode) {
-      case 37:
-        that.leftArrow = true
-        break
-      case 38:
-        that.upArrow = true
-        break
-      case 39:
-        that.rightArrow = true
-        break
-      case 40:
-        that.downArrow = true
-        break
-    }
+	onKeyDown(event){
 
-   this.tagpro_socket.emitKeyboard(that) 
+		let that = this.keys
+		switch (event.keyCode) {
+			case 37:
+				that.leftArrow = true
+				event.preventDefault()
+				break
+			case 38:
+				that.upArrow = true
+				event.preventDefault()
+				break
+			case 39:
+				that.rightArrow = true
+				event.preventDefault()
+				break
+			case 40:
+				that.downArrow = true
+				event.preventDefault()
+				break
+		}
 
-  }
+		this.tagpro_socket.emitKeyboard(that) 
 
-  onKeyUp(event){
-    let that = this.keys
+	}
 
-    switch (event.keyCode) {
-      case 37:
-        that.leftArrow = false
-        break
-      case 38:
-        that.upArrow = false
-        break
-      case 39:
-        that.rightArrow = false
-        break
-      case 40:
-        that.downArrow = false
-        break
-    }
+	onKeyUp(event){
 
-   this.tagpro_socket.emitKeyboard(that) 
+		let that = this.keys
+		switch (event.keyCode) {
+			case 37:
+				that.leftArrow = false
+				event.preventDefault()
+				break
+			case 38:
+				that.upArrow = false
+				event.preventDefault()
+				break
+			case 39:
+				that.rightArrow = false
+				event.preventDefault()
+				break
+			case 40:
+				that.downArrow = false
+				event.preventDefault()
+				break
+		}
 
-  }
+		this.tagpro_socket.emitKeyboard(that) 
+
+	}
 }
